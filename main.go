9a8719993b89004df602f2d21cdc0e0e19e493fc
Martin Beentjes <martin.beentjes@ah.nl>
2019-04-03 21:18:22 +0200

    First setup to log calls

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case "GET":
+
+		fmt.Fprintf(w, "GET\n")
+	case "PUT":
+		fmt.Fprintf(w, "PUT\n")
+	}
+
+	reqDump, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(reqDump))
+}
 
 func main() {
 	fmt.Println("quick")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
